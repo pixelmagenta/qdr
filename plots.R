d5e519d9c5824d057cf1b597d2598795dca53dc4
pixelmagenta <eugustinova@gmail.com>
2019-05-21 23:12:48 +0300

    multi-box-plots changed

--- plots.R
+++ plots.R
@@ -43,18 +43,29 @@ ggplot(metadata, aes(x=metadata$numOfSpeakers, y=metadata$cor_coeff))+
   ggtitle("German")+
   theme(plot.title = element_text(hjust = 0.5))
 
+percentages <- percentages_df
+
+percentages <- data.frame(percentages$group,
+                             percentages$degree,
+                             percentages$w_degree,
+                             percentages$closeness,
+                             percentages$betweenness,
+                             percentages$eigenvector,
+                             percentages$num_words,
+                             percentages$num_sp,
+                             percentages$num_stages)
 
 percentages_df <- data.frame(percentages_df$group,
-                             percentages_df$degree,
-                             percentages_df$w_degree,
-                             percentages_df$closeness,
-                             percentages_df$betweenness,
-                             percentages_df$eigenvector,
-                             percentages_df$num_words,
-                             percentages_df$num_sp,
-                             percentages_df$num_stages)
+                                 percentages_df$degree,
+                                 percentages_df$w_degree,
+                                 percentages_df$closeness,
+                                 percentages_df$betweenness,
+                                 percentages_df$eigenvector,
+                                 percentages_df$num_words,
+                                 percentages_df$num_sp,
+                                 percentages_df$num_stages)
 
-names(percentages_df) <- c("group",
+names(percentages) <- c("group",
                            "Degree",
                            "Weighted degree",
                            "Closeness centrality",
@@ -64,7 +75,7 @@ names(percentages_df) <- c("group",
                            "Number of speech acts", 
                            "Number of stages")
 
-percentages_df2 <- melt(percentages_df, id.vars = c("group"))
+percentages_df2 <- melt(percentages, id.vars = c("group"))
 
 theme_set(theme_minimal(base_size = 14))
 percentages_plot <- ggplot(percentages_df2, aes(x=group, y=value, fill = group)) + 
@@ -80,8 +91,7 @@ percentages_plot <- ggplot(percentages_df2, aes(x=group, y=value, fill = group))
 percentages_plot + facet_wrap(percentages_df2$variable, nrow = 8)
 #+ ggtitle("RusDraCor")
 
-
-
+theme_set(theme_minimal(base_size = 14))
 percentages_plot <- ggplot(percentages_df2, aes(x=group, y=value, fill = group)) + 
   geom_bar(stat = "identity") +
   scale_fill_manual(values=carto_pal(7, "Emrld"))+
@@ -93,32 +103,17 @@ percentages_plot <- ggplot(percentages_df2, aes(x=group, y=value, fill = group))
 
 percentages_plot + facet_wrap(percentages_df2$variable, nrow = 8) + ggtitle("GerDraCor")
 
-theme_set(theme_gray(base_size = 18))
-ggplot(major_group_df, aes(x=year, y=degree)) + 
+
+theme_set(theme_minimal(base_size = 18))
+ggplot(major_group_df, aes(x=year, y=num_words)) + 
   geom_boxplot(outlier.shape = NA) +
-  stat_summary(fun.y=median, geom="smooth", aes(group=0),lwd=1) +
+  stat_summary(fun.y=median, geom="smooth", aes(group=0),lwd=1, color = "green") +
+  labs(x="Decades", y = "Percentage of characters in the Major group")+
   geom_jitter()
 
 
 
 
-
-
-ranking_for_plots <- function(x){
-  df <- data.frame(x$cast, stringsAsFactors = F)
-  for (col in names(x)[-1]){
-    df$t <- rank(x[col], ties.method = "last")
-    names(df)[names(df) == 't'] <- paste0(col, "_rank")
-  }
-  df$text <- rank(rowMeans(subset(df, select=c("num_words_rank", "num_sp_rank", "num_stages_rank"))), ties.method = "max")
-  df$network <- rank(rowMeans(subset(df, select=c("betweenness_rank", "closeness_rank", "w_degree_rank", "degree_rank", "eigenvector_rank"))), ties.method = "max")
-  df$overall <- rank(rowMeans(subset(df, select=c("network", "text"))), ties.method = "max")
-  names(df$x.cast) <- "cast"
-  return(df)
-}
-
-plot_ranks_df <- lapply(metrics_df, ranking_for_plots)
-names(plot_ranks_df) <- metadata$name
 
 
 s <- "babel-marija"
@@ -129,11 +124,11 @@ View(ranks_df[[s]])
 View(plot_ranks_df[[s]])
 
 V(g)$color <- kovesi.linear_bmy_10_95_c71(max(metrics_df[[s]]$eigenvector*10))[V(g)$eigenvector]
-plot_colors <- kovesi.linear_bmy_10_95_c71(max(metrics_df[[s]]$eigenvector*10))[V(g)$eigenvector*10]
+plot_colors <- kovesi.linear_bmy_10_95_c71(max(V(g)$eigenvector))[V(g)$eigenvector]
 
 plot_colors <- kovesi.linear_bmy_10_95_c71(max(metrics_df[[s]]$betweenness))[V(g)$betweenness/10]
 
-plot_colors <- kovesi.linear_bmy_10_95_c71(vcount(g))[V(g)$w_degree]
+plot_colors <- kovesi.linear_bmy_10_95_c71(vcount(g))[V(g)$degree]
 
 V(g)$color <- plot_colors
 V(g)$frame.color <- plot_colors
@@ -145,3 +140,6 @@ plot(g, vertex.label.dist = 1.3, vertex.size = 11, vertex.label.family = "Times"
 
 
 write.csv(l, file = paste0("layout_best_dh_", s, ".csv"))
+
+l33 <- read.csv(file="layout_best_dh_babel-marija.csv")
+l33_m <- data.matrix(l33[2:3])
